483fd1719803b82be76f790a914e9e620d41753f
JULIANO TAVARES DA SILVA <juliano_t_da-silva@estudante.sesisenai.org.br>
2024-07-01 12:06:29 -0300

    first commit

---
+++ revisao/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import BuscarCep from './componets/BuscarCep'
+
+function App() {
+  return (
+    <>
+      <div>
+        <BuscarCep/> 
+      </div>
+    </>
+  )
+}
+
+export default App
